e4b00a6e5af2410a402cef81e1daee4ce1498574
Alexandr Sosnovsky <theone222333111aaa@gmail.com>
2020-07-15 14:28:31 +0300

    update test

--- src/test/java/com/justfors/docker/DockerApplicationTests.java
+++ src/test/java/com/justfors/docker/DockerApplicationTests.java
@@ -7,7 +7,9 @@ import com.squareup.okhttp.ResponseBody;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -16,7 +18,7 @@ import java.util.concurrent.Future;
 
 class DockerApplicationTests {
 
-    private int threadPoolSize = 3;
+    private int threadPoolSize = 300;
 
     private ExecutorService executorService = Executors.newFixedThreadPool(threadPoolSize);
 
@@ -33,15 +35,24 @@ class DockerApplicationTests {
     }
 
     private void doTest() throws Exception {
-        List<Future<List<Long>>> results = executorService.invokeAll(generateLoad(threadPoolSize, 1));
+        List<Future<RequestLatencyMeter.StatisticData>> results = executorService.invokeAll(generateLoad(threadPoolSize, 1));
+        Map<String, Long> serverMap = new HashMap<>();
         double avgltc = results.stream().map(f -> {
             try {
-                return f.get().stream().mapToDouble(a -> a).average().getAsDouble();
+                RequestLatencyMeter.StatisticData statisticData = f.get();
+                Long count = serverMap.get(statisticData.response);
+                if (count != null) {
+                    serverMap.put(statisticData.response, ++count);
+                } else {
+                    serverMap.put(statisticData.response, 1L);
+                }
+                return statisticData.totalTime;
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
-            return 0d;
+            return 0L;
         }).mapToDouble(a -> a).average().getAsDouble();
+        serverMap.forEach((k,v) -> System.out.println(k + "\n" + "count of call: " + v));
         System.out.println(avgltc + "   avg msec");
     }
 
@@ -54,7 +65,7 @@ class DockerApplicationTests {
     }
 
 
-    private class RequestLatencyMeter implements Callable<List<Long>> {
+    private class RequestLatencyMeter implements Callable<RequestLatencyMeter.StatisticData> {
 
         private long requestsCount;
 
@@ -63,25 +74,33 @@ class DockerApplicationTests {
         }
 
         @Override
-        public List<Long> call() {
-            List<Long> latencies = new ArrayList<>();
+        public RequestLatencyMeter.StatisticData call() {
+            long start = System.currentTimeMillis();
+            String response = null;
             for (int j = 0; j < requestsCount; j++) {
-                long start = System.currentTimeMillis();
-                String response = makeRequest();
-                System.out.println(response);
-                long end = System.currentTimeMillis();
-                latencies.add(end - start);
+                response = makeRequest();
             }
-            return latencies;
+            long end = System.currentTimeMillis();
+            return new StatisticData((end - start), response);
+        }
+
+        private class StatisticData {
+            long totalTime;
+            String response;
+
+            StatisticData(long totalTime, String response) {
+                this.totalTime = totalTime;
+                this.response = response;
+            }
         }
     }
 
     private String makeRequest() {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("http://localhost:7000")
+                //.url("http://localhost:7000")
                 //.url("http://192.168.99.100:8989")
-                //.url("http://192.168.99.101:7777")
+                .url("http://192.168.99.101:7777")
                 .method("GET", null)
                 .build();
 
